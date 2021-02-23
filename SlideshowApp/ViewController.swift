85f01756aad0e9ccab66fdda95e33cd3ff96babb
megumi.K <daron.d.libra@gmail.com>
2021-02-23 18:38:03 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 片桐めぐみ on 2021/02/23.
+//  Copyright © 2021 megumi.katagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
